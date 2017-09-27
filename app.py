2f355973d2682e8c1032f0cebd62de6a5e469605
vagrant <vagrant@vagrant.vm>
2017-09-27 19:38:01 +0000

    add app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from catalog import app
+from catalog import main
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(debug=True, use_reloader=True)
+    app.debug = True
+    app.run(host='0.0.0.0', port=8085)
